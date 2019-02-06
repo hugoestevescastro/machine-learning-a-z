542f609fba07c359eac7f0b2ba656a3bd59a82fd
Hugo Esteves de Castro <inzone23@outlook.com>
2019-02-06 18:26:12 +0000

    X_grid template

--- resolução/Part 2 - Regression/Section 6 - Polynomial Regression/regression_template.py
+++ resolução/Part 2 - Regression/Section 6 - Polynomial Regression/regression_template.py
@@ -17,8 +17,10 @@ y = dataset.iloc[:, 2].values
 ## NEED TO CREATE THE REGRESSOR
 
 # Visualising the Regression Model results
+X_grid = np.arrange(min(X), max(X), 0.1)
+X_grid = X_grid.reshape((len(X_grid), 1))
 plt.scatter(X, y, color='red')
-plt.plot(X, regressor.predict(X), color='blue')
+plt.plot(X_grid, regressor.predict(X_grid), color='blue')
 plt.title("Truth or Bluff (Polynomial Regression)")
 plt.xlabel('Position Level')
 plt.ylabel('Salary')
